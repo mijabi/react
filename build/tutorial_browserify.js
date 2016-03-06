224147a7d5feba7d8f340ebc42f96bf0afabce5e
Masayuki Sato <mijabi@gmail.com>
2016-03-06 19:51:51 +0900

    edit gulpfile.js

--- build/tutorial_browserify.js
+++ build/tutorial_browserify.js
@@ -1,4 +1,7 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var marked = require('marked');
+
 var $ = require('jquery');
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -12,33 +12,7 @@ gulp.task('browserify', function(){
   return b.bundle()
     .pipe(source('tutorial_browserify_compiled.js'))
         // .pipe(uglify({
-        //     preserveComments: 'license' // ライセンスコメントを残しつつminify
+        //     preserveComments: 'license'
         // }))
     .pipe(gulp.dest('./build'));
 });
-
-// var gulp = require('gulp');
-// var browserify = require('browserify');
-// var source = require('vinyl-source-stream');
-// var buffer = require('vinyl-buffer');
-// var uglify = require('gulp-uglify');
-
-// gulp.task('build-js', function () {
-//   "browserify": {
-//     "transform": [
-//       ["reactify", {"harmony": true} ]
-//     ]
-//   }
-
-
-//     return browserify({
-//         entries: './src/js/app.js' // どのファイルからビルドするか
-//     }).plugin('licensify') // licensifyプラグインの有効化
-//         .bundle() // browserifyの実行
-//         .pipe(source('app.js'))
-//         .pipe(buffer())
-//         .pipe(uglify({
-//             preserveComments: 'license' // ライセンスコメントを残しつつminify
-//         }))
-//         .pipe(gulp.dest('./static/js')); // 出力
-// });
